f4110595921760c508cf805f0f05dc242b1459c1
sumin <tnals2565@naver.com>
2025-06-09 22:08:21 +0900

    Feat/loading UI (#7)
    * feat: skeleton-card 컴포넌트 세팅
    
    * feat: 추천 와인 카드 스켈레톤 UI 모바일 완료
    
    * feat: 와인 리스트 카드 스켈레톤 UI 모바일 완료
    
    * feat: 와인 상세 상단 카드 스켈레톤 UI 모바일 완료
    
    * feat: 와인 상세 리스트 카드 스켈레톤 UI 모바일 완료
    
    * feat: 로딩스피너 UI 구현 완료
    
    * feat: skeleton-card-loader 컴포넌트 작업 완료
    
    * feat: spinner-loader 컴포넌트 작업 완료
    
    * feat: 로딩 UI 컴포넌트 디렉토리 변경
    
    * feat: 루트레벨 loading 테스트 추가 완료
    
    * refactor: skeleton 및 spinner 컴포넌트 파일명 수정
    
    * refactor: arrow function으로 컨벤션 수정
    
    * refactor: function 컨벤션에 따른 import 수정
    
    * refactor: 테스트 코드 삭제
    
    * refactor: 스켈레톤 UI isLoading 상태에서만 사용되도록 수정
    
    * refactor: pr 수정사항 적용 완료
    
    * feat: components 디렉토리 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,11 @@
+import Spinner from '@/shared/components/common/spinner';
+
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <Spinner />
+    </div>
+  );
 };
 
 export default Loading;
---
+++ src/shared/components/common/spinner/index.tsx
@@ -0,0 +1,38 @@
+import { cn } from '@/shared/libs/utils/cn';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+const spinnerVariants = cva(
+  'inline-block animate-[spin_0.6s_linear_infinite] rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]',
+  {
+    variants: {
+      size: {
+        small: 'h-[1.6rem] w-[1.6rem]', // 16px
+        large: 'h-[2.4rem] w-[2.4rem]', // 32px
+      },
+      color: {
+        primary: 'text-primary',
+        secondary: 'text-secondary',
+      },
+    },
+    defaultVariants: {
+      size: 'small',
+      color: 'primary',
+    },
+  }
+);
+
+type SpinnerProps = {
+  className?: string;
+} & VariantProps<typeof spinnerVariants>;
+
+const Spinner = ({ size, className, color }: SpinnerProps) => {
+  return (
+    <div className={cn(spinnerVariants({ size, color }), className)} role="status">
+      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+        Loading...
+      </span>
+    </div>
+  );
+}
+
+export default Spinner;
