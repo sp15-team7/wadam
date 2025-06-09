35c4675803310db3841b37aa585747314420bfc9
Jaehyun Park <hirouth@naver.com>
2025-06-09 12:11:39 +0900

    feat: Auth.js 에러 메시지 정의

---
+++ src/feature/auth/constants/auth.message.ts
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGES = {
+  INVALID_INPUT: '입력 값이 유효하지 않습니다. 다시 확인해주세요.',
+  SIGN_UP_FAILED:
+    '회원가입에 실패했습니다. 이미 사용 중인 이메일 또는 닉네임일 수 있습니다.',
+  INVALID_CREDENTIALS: '이메일 또는 비밀번호가 올바르지 않습니다.',
+  UNKNOWN: '알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요.',
+};
