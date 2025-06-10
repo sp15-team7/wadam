bed236d00e1c3bd66cbb61748cf5159e15b5f418
Hyun <ikimyh059554@gmail.com>
2025-06-10 14:32:39 +0900

    feat: auth에서 서버 액션의 실행결과로 발생하는 에러메시지 표시 컴포넌트 추가

---
+++ src/feature/auth/components/ErrorMessage.tsx
@@ -0,0 +1,19 @@
+/**
+ * @author Hyun
+ * @since 2025-06-10
+ * @description: 에러 메시지 컴포넌트 (서버 액션의 실행결과로 발생하는 에러메시지를 표시하는 로직)
+ */
+
+interface ErrorMessageProps {
+  message?: string;
+}
+
+const ERROR_MESSAGE_STYLE = 'text-destructive text-sm font-medium';
+
+const ErrorMessage = ({ message }: ErrorMessageProps) => {
+  if (!message) return null;
+
+  return <p className={ERROR_MESSAGE_STYLE}>{message}</p>;
+};
+
+export default ErrorMessage;
