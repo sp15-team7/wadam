af46a7b072293c78c8e24b538c27b40a13e5824b
sumin <tnals2565@naver.com>
2025-06-10 16:31:36 +0900

    refactor: test code 확인 후 삭제

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,13 +1,6 @@
-'use client';
-import StarRating from '@/shared/components/common/star-rating';
-import { useState } from 'react';
-
 const LandingPage = () => {
-  const [rating, setRating] = useState(3);
   return (
-    <div>
-      <StarRating value={rating} onChange={setRating} readOnly={false} />
-    </div>
+    <div>LandingPage</div>
   );
 };
 export default LandingPage;
