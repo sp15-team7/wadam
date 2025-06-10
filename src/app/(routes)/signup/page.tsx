c104fb5bde480d9cfcb711e0d2e522b4b3cf5e3c
Hyun <ikimyh059554@gmail.com>
2025-06-10 16:08:59 +0900

    회원가입 페이지 구조 수정 (main 추가)

--- src/app/(routes)/signup/page.tsx
+++ src/app/(routes)/signup/page.tsx
@@ -1,5 +1,9 @@
 import SignUpForm from '@/feature/auth/components/SignUpForm';
 
 export default async function SignUpPage() {
-  return <SignUpForm />;
+  return (
+    <main className='flex-center h-dvh w-dvw'>
+      <SignUpForm />;
+    </main>
+  );
 }
