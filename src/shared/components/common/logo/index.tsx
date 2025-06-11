4883675d63fe8b3e4adbb0ea4b681875081bde93
Jaehyun Park <hirouth@naver.com>
2025-06-11 19:33:59 +0900

    perf: logo 컴포넌트 성능 개선

--- src/shared/components/common/logo/index.tsx
+++ src/shared/components/common/logo/index.tsx
@@ -1,4 +1,19 @@
-const Logo = () => {
-  return <div>index</div>;
+import Image from 'next/image';
+
+const Logo = ({ color = 'red' }: { color?: 'red' | 'ivory' }) => {
+  const logoSrc =
+    color === 'red' ? '/logos/logo-red.svg' : '/logos/logo-ivory.svg';
+
+  return (
+    <Image
+      src={logoSrc}
+      alt='logo'
+      width={150}
+      height={27.8}
+      priority
+      className='h-[2.78rem] w-[15rem] md:h-[5.57rem] md:w-[30rem]'
+    />
+  );
 };
+
 export default Logo;
