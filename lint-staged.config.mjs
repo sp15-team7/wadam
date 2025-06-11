35d0e44e137989a2f34c21a2a69f2a680168966a
Jaehyun Park <hirouth@naver.com>
2025-06-12 00:53:33 +0900

    feat: 린트 스테이지 자동 픽스 기능 추가

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,7 @@
 import path from 'path';
 
 const buildEslintCommand = (filenames) =>
-  `next lint --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file  ')}`;
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file  ')}`;
 
 export default {
   '**/*.{js,jsx,ts,tsx}': [
