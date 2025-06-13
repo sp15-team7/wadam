1699802babe58f930b251e878a4ff20736d52bbb
Hyun <ikimyh059554@gmail.com>
2025-06-13 22:27:39 +0900

    feat: 와인 향 enum 타입 구현

---
+++ src/shared/types/flavor-label.ts
@@ -0,0 +1,28 @@
+/**
+ * @author: Hyun
+ * @since: 2025-06-13
+ * @description: 와인 향 타입 정의
+ */
+
+// 와인 향 필터링 컴포넌트 영어 라벨 타입
+export enum FlavorLabel {
+  '체리' = 'CHERRY',
+  '베리' = 'BERRY',
+  '오크' = 'OAK',
+  '바닐라' = 'VANILLA',
+  '후추' = 'PEPPER',
+  '제빵' = 'BAKING',
+  '풀' = 'GRASS',
+  '사과' = 'APPLE',
+  '복숭아' = 'PEACH',
+  '시트러스' = 'CITRUS',
+  '트로피컬' = 'TROPICAL',
+  '미네랄' = 'MINERAL',
+  '꽃' = 'FLOWER',
+  '담뱃잎' = 'TOBACCO',
+  '흙' = 'EARTH',
+  '초콜릿' = 'CHOCOLATE',
+  '스파이스' = 'SPICE',
+  '카라멜' = 'CARAMEL',
+  '가죽' = 'LEATHER',
+}
