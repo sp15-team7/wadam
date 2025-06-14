1357f11768ed847ead06b2caa5df66f2ef2df05a
spiffreact <spiffreact@gmail.com>
2025-06-14 20:57:32 +0900

    feat: 로그인 페이지 스타일 및 UI 개선 (#55)
    * feat: 로그인폼 간격 수정
    
    * feat: 카카오톡 아이콘 버튼에 추가
    
    * feat: 로그인 및 카카오 버튼 폰트수정
    
    * feat: 이메일 및 비밀번호 폰트수정
    
    * feat: 카카오 로고 -> 아이콘으로 변경
    
    * feat: 로딩스피너 복구
    
    * feat: 회원가입 페이지 폼과버튼간의 간격수정
    
    * feat: 회원가입 페이지 에러메세지 수정
    
    * feat: 에러메세지 발생시 입력창 이동수정
    
    * feat: dropdown menu combobox 로 구현
    
    * feat: signin 페이지 수정
    
    * feat: 드랍다운 컴포넌트 삭제
    
    * feat: 에러메세지 발생시 입력창 이동 문제 해결
    
    * feat: signin form
    
    ---------
    
    Co-authored-by: Jin <gogomose@gmail.com>
    Co-authored-by: Jaehyun Park <hirouth@naver.com>

--- src/app/(entry)/signup/page.tsx
+++ src/app/(entry)/signup/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from '@/feature/auth/components/SignUpForm';
 
 export default async function SignUpPage() {
   return (
-    <main className='flex-center h-dvh w-dvw flex-col'>
+    <main className='flex h-full items-center justify-center'>
       <SignUpForm />
     </main>
   );
--- src/feature/auth/components/SignInForm.tsx
+++ src/feature/auth/components/SignInForm.tsx
@@ -1,8 +1,6 @@
-// src/feature/auth/components/SignInForm.tsx
-
 /**
  * @author: Jin
- * @since: 2025-06-10
+ * @since: 2025-06-12
  * @description: 로그인 폼 컴포넌트 (react-hook-form -> zod -> server action)
  *
  * handleSubmit: 폼 제출 시 동작, onSubmit 함수 호출
@@ -12,6 +10,7 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import Image from 'next/image';
 import { JSX, useActionState, useTransition } from 'react';
 import { useForm } from 'react-hook-form';
 
@@ -20,6 +19,7 @@ import {
   type SignInFormData,
   signInSchema,
 } from '@/feature/auth/schema/auth.schema';
+import Logo from '@/shared/components/common/logo';
 import Spinner from '@/shared/components/common/spinner';
 
 import { Button } from '../../../shared/components/ui/button';
@@ -30,7 +30,7 @@ import FormField from './FormField';
 
 const SignInForm: () => JSX.Element = () => {
   const [state, formAction] = useActionState(signInAction, null);
-  const [isPending, startTransition] = useTransition();
+  const [_isPending, startTransition] = useTransition();
 
   const {
     register,
@@ -54,7 +54,7 @@ const SignInForm: () => JSX.Element = () => {
 
   return (
     <>
-      {isPending && (
+      {_isPending && (
         <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/70'>
           <div className='flex flex-col items-center gap-4 rounded-lg bg-white p-8 shadow-lg'>
             <Spinner size='large' color='primary' />
@@ -62,43 +62,59 @@ const SignInForm: () => JSX.Element = () => {
           </div>
         </div>
       )}
+
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='flex-center flex h-[67.9rem] w-[34.3rem] flex-col gap-5 px-4 md:h-[76.2rem] md:w-[49.6rem] lg:h-[79.4rem] lg:w-[50rem]'
+        className='flex h-[67.9rem] w-[34.3rem] flex-col items-center gap-5 px-4 pt-36 md:h-[76.2rem] md:w-[49.6rem] lg:h-[79.4rem] lg:w-[50rem]'
       >
-        <FormField<SignInFormData>
-          label='이메일'
-          name='email'
-          type='email'
-          placeholder='이메일 입력'
-          register={register}
-          errors={errors}
-        />
+        <div className='mb-20 flex'>
+          <Logo className='' />
+        </div>
+        <div className='flex w-full flex-col gap-8 pt-13'>
+          <FormField<SignInFormData>
+            label='이메일'
+            name='email'
+            type='email'
+            placeholder='이메일 입력'
+            register={register}
+            errors={errors}
+          />
 
-        <FormField<SignInFormData>
-          label='비밀번호'
-          name='password'
-          type='password'
-          placeholder='비밀번호 입력'
-          register={register}
-          errors={errors}
-        />
+          <FormField<SignInFormData>
+            label='비밀번호'
+            name='password'
+            type='password'
+            placeholder='비밀번호 입력'
+            register={register}
+            errors={errors}
+          />
+        </div>
+        <div className='txt-md-bold flex w-full flex-col gap-8 pt-61'>
+          <SubmitButton>로그인</SubmitButton>
+          <Button
+            size='full'
+            className='txt-md-bold bg-[#FFDB00] !text-black hover:bg-[#FFDB00]/60'
+          >
+            <div className='flex-center gap-[2rem]'>
+              <Image
+                src='/icons/ui/icon-kakao.svg'
+                alt='카카오 아이콘'
+                width={24}
+                height={24}
+                className='h-[2rem] w-[2rem] md:h-[2.4rem] md:w-[2.4rem] lg:h-[2.8rem] lg:w-[2.8rem]'
+              />
+              카카오로 시작하기
+            </div>
+          </Button>
 
-        <SubmitButton isPending={isPending}>로그인</SubmitButton>
-        <Button
-          size='full'
-          className='bg-[#FFDB00] !text-black hover:bg-[#FFDB00]/60'
-        >
-          카카오로 시작하기
-        </Button>
+          {state?.message && <ErrorMessage message={state.message} />}
 
-        {state?.message && <ErrorMessage message={state.message} />}
-
-        <AuthLink
-          label='계정이 없으신가요?'
-          linkText='회원가입하기'
-          href='/signup'
-        />
+          <AuthLink
+            label='계정이 없으신가요?'
+            linkText='회원가입하기'
+            href='/signup'
+          />
+        </div>
       </form>
     </>
   );
