5dad08aef24b967151734b3f784dc9d2167fdcdb
Hyun <ikimyh059554@gmail.com>
2025-06-15 14:52:13 +0900

    refactor: 자동 정렬 규칙에 맞도록 코드 위치 수정

--- src/shared/components/ui/dual-range-slider.tsx
+++ src/shared/components/ui/dual-range-slider.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import * as React from 'react';
-import * as SliderPrimitive from '@radix-ui/react-slider';
-
 import { cn } from '@/shared/libs/utils/cn';
+import * as SliderPrimitive from '@radix-ui/react-slider';
+import * as React from 'react';
 
 interface DualRangeSliderProps
   extends React.ComponentProps<typeof SliderPrimitive.Root> {
