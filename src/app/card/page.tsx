57172ef5befc450d5e51dcbdaf5053a9c1195e3f
Jaehyun Park <hirouth@naver.com>
2025-06-15 23:46:31 +0900

    feat: 상세 카드 1차 구현

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -1,12 +1,14 @@
+import DetailCard from '@/feature/wines/components/DetailCard';
 import MonthlyCard from '@/feature/wines/components/MonthlyCard';
 import WineCard from '@/feature/wines/components/WineCard';
 
 const CardPage = () => {
   return (
-    <>
-      <WineCard />
+    <div className='flex flex-col gap-6'>
       <MonthlyCard />
-    </>
+      <WineCard />
+      <DetailCard />
+    </div>
   );
 };
 export default CardPage;
---
+++ src/feature/wines/components/DetailCard.tsx
@@ -0,0 +1,41 @@
+import { EllipsisVertical } from 'lucide-react';
+import Image from 'next/image';
+
+import { Card, CardContent } from '@/shared/components/ui/card';
+
+const DetailCard = () => {
+  return (
+    <Card className='h-[19rem] w-[34.3rem] md:h-[26rem] md:w-[70.4rem] lg:w-[114rem]'>
+      <CardContent className='flex h-full px-6 md:gap-8'>
+        <figure className='flex items-end px-4 md:px-6'>
+          <Image
+            src={'/images/wines/image-wine1.png'}
+            alt='와인 이미지'
+            width={58}
+            height={209}
+            className='md:h-[30.2rem] md:w-[8.4rem]'
+          />
+        </figure>
+        <article className='relative flex flex-2 flex-col justify-between gap-2 py-12'>
+          <EllipsisVertical
+            size={28}
+            color='#ac271e'
+            className='absolute top-12 right-0'
+          />
+          <div className='flex flex-col gap-4'>
+            <h2 className='flex max-w-[19rem] items-start text-[2rem] leading-tight font-semibold md:max-w-[30rem] md:text-[3rem]'>
+              Sentinel Carbernet Sauvignon 2016
+            </h2>
+            <p className='text-[1.6rem]'>Western Cape, South Africa</p>
+          </div>
+          <div className=''>
+            <strong className='text-primary bg-secondary w-fit rounded-full px-[1rem] py-[0.6rem] text-[1.4rem] font-bold md:text-[1.8rem]'>
+              ₩ 64,990
+            </strong>
+          </div>
+        </article>
+      </CardContent>
+    </Card>
+  );
+};
+export default DetailCard;
