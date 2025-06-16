0c3b8903826f6a239627327b5b5b7c78948bcb38
SarangOh <sarang7180@kakao.com>
2025-06-16 16:49:11 +0900

    feat: radio-input 스타일 일부 수정

--- src/shared/components/common/radio-input/index.tsx
+++ src/shared/components/common/radio-input/index.tsx
@@ -31,7 +31,6 @@ const RadioInput = React.forwardRef<
       radioClassName,
       labelClassName,
       value, // RadioGroupItem의 value를 받음
-      onValueChange,
       ...props
     },
     ref,
@@ -43,30 +42,11 @@ const RadioInput = React.forwardRef<
         className={cn(
           'flex items-center', // 레이아웃
           'gap-[1.5rem]', // 체크박스와 텍스트 사이 간격
-          'p-[1.2rem]',
+          'p-[1.2rem]', // 내부 패딩
           'rounded-lg',
-          'hover:bg-primary cursor-pointer transition-colors duration-200', // 호버 효과
+          'cursor-pointer duration-200', // 호버 효과
           containerClassName,
         )}
-        onClick={() => {
-          if (value && onValueChange) {
-            onValueChange(value);
-          }
-        }}
-        // 키보드 접근성 추가
-        onKeyDown={(event) => {
-          if (
-            event.key === 'Enter' ||
-            event.key === ' ' ||
-            event.key === 'Spacebar'
-          ) {
-            event.preventDefault(); // 기본 스크롤 동작 방지
-            if (value && onValueChange) {
-              onValueChange(value);
-            }
-          }
-        }}
-        tabIndex={0} // 키보드 포커스 가능하게
       >
         <RadioGroupItem
           ref={ref}
@@ -75,24 +55,43 @@ const RadioInput = React.forwardRef<
           className={cn(
             'h-[2rem] w-[2rem]', // 체크박스 크기 20*20
             'rounded-[6px]',
-            'border-[1px] border-[var(--color-gray)]',
-            'bg-[var(--color-secondary)]', // 체크박스 배경색
-            'data-[state=checked]:border-[var(--color-secondary)]', // 선택 시 테두리 색상 변경
-            'text-secondary', // 체크박스 내부 체크 점 색상
-            'focus:ring-offset-background focus:ring-ring focus:ring-2 focus:ring-offset-2', // 포커스 스타일 유지
+            'border-[1px] border-[var(--color-white)]',
+            'bg-[var(--color-white)]', // 체크박스 배경색
+            'overflow-hidden',
+            'relative', // ::after 가상 요소를 위해 relative 설정
+            // 체크되었을 때의 스타일
+            'data-[state=checked]:border-[--color-secondary-light]', // 선택 시 테두리 색상
+            'data-[state=checked]:bg-[var(--color-white)]', // 선택 시 내부 채움 색상을 흰색으로 유지
+            'data-[state=checked]:text-transparent',
+
+            // === 내부 체크박스 css ===
+            'data-[state=checked]:after:content-[""]', // 체크 시 ::after 가상 요소 활성화
+            'data-[state=checked]:after:block',
+            'data-[state=checked]:after:w-[1rem]',
+            'data-[state=checked]:after:h-[1rem]',
+            'data-[state=checked]:after:rounded',
+            'data-[state=checked]:after:bg-primary',
+            'data-[state=checked]:after:absolute',
+            'data-[state=checked]:after:top-1/2',
+            'data-[state=checked]:after:left-1/2',
+            'data-[state=checked]:after:-translate-x-1/2',
+            'data-[state=checked]:after:-translate-y-1/2',
+            // ===============================================
+
+            'focus-visible:ring-ring focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none',
+            'cursor-pointer',
             radioClassName,
             className,
           )}
           {...props}
-        >
-          {/* RadioGroupItem의 내부 체크 마크는 CSS로 제어합니다. */}
-        </RadioGroupItem>
+        ></RadioGroupItem>
         <label
           htmlFor={uniqueId}
           id={`label-${uniqueId}`} // aria-labelledby와 연결되도록 id 연결
           className={cn(
-            'txt-lg-regular',
+            'txt-lg-bold',
             'txt-black', // 텍스트 색상
+            'cursor-pointer',
             'peer-disabled:cursor-not-allowed peer-disabled:opacity-70', // disabled 스타일 유지
             labelClassName,
           )}
