de1476c5a2b79d57e9cc69b50ece7e739919b041
sumin <tnals2565@naver.com>
2025-06-17 21:50:53 +0900

    feat: wine progress 컴포넌트 구조 수정

--- src/feature/wines/components/wine-progress/index.tsx
+++ src/feature/wines/components/wine-progress/index.tsx
@@ -21,6 +21,21 @@ const mockData = {
   reviewCount: 55,
 };
 
+const getAverageRating = (data: typeof mockData) => {
+  const { avgRatings, reviewCount } = data;
+
+  if (!reviewCount || reviewCount === 0) return 0;
+
+  const totalScore = Object.entries(avgRatings).reduce(
+    (acc, [rating, count]) => {
+      return acc + Number(rating) * count;
+    },
+    0,
+  );
+
+  return (totalScore / reviewCount).toFixed(1); // 소수점 첫째자리까지 반올림
+};
+
 const WineProgress = ({ wineId }: WineProgressProps) => {
   console.log(wineId);
   // TODO: API 연동 시 windId를 받아 커스텀 훅으로 데이터 가져와 사용
@@ -29,9 +44,14 @@ const WineProgress = ({ wineId }: WineProgressProps) => {
 
   return (
     <div>
-      <p className='txt-2xl-bold'>
-        {mockData.reviewCount.toLocaleString('ko-KR')}개의 후기
-      </p>
+      <div className='flex items-center gap-[2rem]'>
+        <strong className='text-[5.4rem] font-extrabold'>
+          {getAverageRating(mockData)}
+        </strong>
+        <div>
+          <p>{mockData.reviewCount.toLocaleString('ko-KR')}개의 후기</p>
+        </div>
+      </div>
       <div className='mt-[2rem] flex flex-col gap-[1.5rem]'>
         {[5, 4, 3, 2, 1].map((score) => (
           <div key={score} className='flex items-center gap-[1.5rem]'>
