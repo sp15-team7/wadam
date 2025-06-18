6a3b85df1474896f457810a102b9ecd52b4cf226
Hyun <ikimyh059554@gmail.com>
2025-06-18 21:55:04 +0900

    refactor: 테스트 코드 삭제 후 기본 동작 코드로 수정

--- src/app/(main)/myprofile/page.tsx
+++ src/app/(main)/myprofile/page.tsx
@@ -1,3 +1,9 @@
+/**
+ * @author: Hyun
+ * @since: 2025-06-18
+ * @description: 마이페이지(마이프로필) 라우팅 페이지로, 로그인 상태일 때만 접근 가능(세션 정보를 넘겨줌)
+ */
+
 import { auth } from '@/feature/auth/libs/auth';
 import MyPage from '@/feature/myprofile/components/MyPage';
 
