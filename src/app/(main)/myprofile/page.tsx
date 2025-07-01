f505e24c076426f108273fb62f98ab67666cf0b3
Hyun <ikimyh059554@gmail.com>
2025-07-01 19:59:50 +0900

    refactor: 주석 수정(마이페이지)

--- src/app/(main)/myprofile/page.tsx
+++ src/app/(main)/myprofile/page.tsx
@@ -1,7 +1,7 @@
 /**
  * @author: Hyun
  * @since: 2025-06-18
- * @description: 마이페이지(마이프로필) 라우팅 페이지로, 로그인 상태일 때만 접근 가능(세션 정보를 넘겨줌)
+ * @description: 마이페이지(마이프로필) 라우팅 페이지로, 로그인 상태일 때만 접근 가능
  */
 
 import MyPage from '@/feature/myprofile/components/MyPage';
